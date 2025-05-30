f2b4a65e9482afad0ab80fb86d5dfb4251ec3b83
Israel <israel.oliversw@gmail.com>
2025-05-30 09:48:04 -0300

    Arrumando a animação no mobile

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let pag1 = document.getElementById("pag1");
 let pag2 = document.getElementById("pag2");
 let pag3 = document.getElementById("pag3");
-let content = document.querySelector("content ")
+let isMobile = window.matchMedia("(max-width: 767px)").matches;
 
 let pages = document.querySelector(".rightContent2");
 
-pages.addEventListener("mouseenter", () => {
-    if(content.style.flexDirection == column) {
-        pag1.style.transform = "translateX(0)";
-        pag2.style.transform = "translateX(0)";
-        pag3.style.transform = "translateX(0)";
-    } else {
+if (!isMobile) {
+    pages.addEventListener("mouseenter", () => {
         pag1.style.transform = "translateX(-100px)";
         pag2.style.transform = "translateX(-10px)";
         pag3.style.transform = "translateX(70px)";
-    }
-});
+    });
+}
 
 pages.addEventListener("mouseleave", () => {
     pag1.style.transform = "translateX(0)";
